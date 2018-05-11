6f936de6ea449783df832ada112e4fb630a0c365
Uy Binh <uybinh@gmail.com>
2018-05-11 21:58:37 +0700

    Finish 'update' route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   def destroy
     Article.find(params[:id]).destroy
     redirect_to articles_path
