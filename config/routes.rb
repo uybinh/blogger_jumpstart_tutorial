c33f3f23e173fd99498176edfdba2b915156bd3e
Uy Binh <uybinh@gmail.com>
2018-05-13 14:04:16 +0700

    Add login logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   end
   resources :tags
   root to: "articles#index"
+  resources :authors
+
+  resources :author_sessions, only: [:new, :create, :destroy]
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
